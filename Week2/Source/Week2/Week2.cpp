3591694718ff009b38e43e4d61ef25ef73ebf04d
Jennifer Volk <jenniferevolk@gmail.com>
2022-11-27 09:18:07 -0800

    project base
    setup base to build on...

---
+++ Week2/Source/Week2/Week2.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Week2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Week2, "Week2" );
+ 
