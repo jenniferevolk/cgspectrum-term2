cdcada37ae669cc371a444ef37c7444a3d1723d4
Jennifer Volk <jenniferevolk@gmail.com>
2023-01-13 13:25:43 -0800

    week 7 -cameras

---
+++ week7/Source/doors/Public/AbstractionGameModeBase.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Blueprint/UserWidget.h"
+#include "AbstractionGameModeBase.generated.h"
+
+
+
+
+UCLASS()
+class DOORS_API AAbstractionGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+
+public:
+	void StartPlay() override;
+
+	UPROPERTY(EditDefaultsOnly)
+		TSubclassOf<UUserWidget> ObjectiveWidgetClass;
+
+	UUserWidget* ObjectiveWidget = nullptr;
+};
